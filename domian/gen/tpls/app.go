75b046bec33a26b712c824054e4540157afb9bf0
shaokejie <ucwebos@163.com>
2022-07-05 22:32:18 +0800

    init

--- domian/gen/tpls/app.go
+++ domian/gen/tpls/app.go
@@ -77,7 +77,7 @@ const AppConfigTmplRpcRegisterImpl = `package conf
 
 // RegisterRPCClients 注册Rpc客户端
 func RegisterRPCClients() {
-	//rpc_client.RegisterRPC(conf.Namespace, conf.App.GetEtcd(), rpc_client.User) // 在这里注册依赖的其他服务 注册后 代码中可以直接使用
+	//rpc_client.RegisterRPC(Namespace, App.GetEtcd(), rpc_client.User) // 在这里注册依赖的其他服务 注册后 代码中可以直接使用
 }`
 
 const AppConfigTmpl = `package conf
